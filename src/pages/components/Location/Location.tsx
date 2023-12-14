0e8fdbed8c3c89eceb6ddcbf8b06498c99b5af2f
Vytautas77 <133365323+Vytautas77@users.noreply.github.com>
2023-12-14 11:37:21 +0200

    added correct

--- src/pages/components/Location/Location.tsx
+++ src/pages/components/Location/Location.tsx
@@ -18,22 +18,28 @@ type LocationComponentType = {
 
 const Location: React.FC<LocationComponentType> = ({ location }) => {
   return (
-    <Link className={styles.link} href={`/location/${location._id}`}>
-      <div className={styles.wrapper}>
-        <img
-          className={styles.photo}
-          src={location.location_photo_url}
-          alt={location.title}
-        />
-        <div>
-          <h2>{location.title}</h2>
-          <div className={styles.coordinates}>
-            <span> {location.latitude}</span> <span>{location.longitude}</span>
+    <>
+      {location && (
+        <Link className={styles.link} href={`/location/${location._id}`}>
+          console.log(location._id);
+          <div className={styles.wrapper}>
+            <img
+              className={styles.photo}
+              src={location.location_photo_url}
+              alt={location.title}
+            />
+            <div>
+              <h2>{location.title}</h2>
+              <div className={styles.coordinates}>
+                <span> {location.latitude}</span>{" "}
+                <span>{location.longitude}</span>
+              </div>
+              <p>{location.description}</p>
+            </div>
           </div>
-          <p>{location.description}</p>
-        </div>
-      </div>
-    </Link>
+        </Link>
+      )}
+    </>
   );
 };
 
